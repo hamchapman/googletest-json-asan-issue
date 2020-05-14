84287eeef6e25a5b2e9b17b691d53a2315cfb460
Hamilton Chapman <hamchapman@gmail.com>
2020-05-14 09:51:31 +0100

    Update test file to make issue clearer

--- test/FooTest.cpp
+++ test/FooTest.cpp
@@ -8,14 +8,7 @@ using nlohmann::json;
 
 class FooTests : public ::testing::Test {};
 
-TEST_F(FooTests, Foo1) {}
-TEST_F(FooTests, Foo2) {}
-TEST_F(FooTests, Foo3) {}
-
-// If you uncomment the line below then the buffer overflow won't occur
-TEST_F(FooTests, Foo4) {}
-
-TEST_F(FooTests, Foo) {
+TEST_F(FooTests, Foo1) {
   ASSERT_EQ(foobar::add(1, 2), 3);
 
   // Everything is fine if you use a vector of doubles.
@@ -27,5 +20,11 @@ TEST_F(FooTests, Foo) {
 
   // If you uncomment the line below then the buffer overflow also won't occur
   std::vector<int> fetched_ints = ints_json.get<std::vector<int>>();
-
 }
+
+TEST_F(FooTests, Foo2) {}
+TEST_F(FooTests, Foo3) {}
+TEST_F(FooTests, Foo4) {}
+
+// If you uncomment the line below then the buffer overflow won't occur
+TEST_F(FooTests, Foo5) {}
